FIX: Bug in tracking units.

src/gmprocess/core/stationtrace.py
@@ -13,18 +13,11 @@ from scipy.integrate import cumtrapz
 
 # local imports
 from gmprocess.utils.config import get_config
+from gmprocess.utils import constants
 from gmprocess.io.cosmos.data_structures import BUILDING_TYPES
 from gmprocess.io.seedname import get_units_type
 from gmprocess.core.provenance import TraceProvenance
 
-UNITS = {"acc": "cm/s^2", "vel": "cm/s"}
-REVERSE_UNITS = {
-    "cm/s^2": "acc",
-    "cm/s**2": "acc",
-    "cm/s/s": "acc",
-    "cm/s": "vel",
-    "cm": "disp",
-}
 
 PROCESS_LEVELS = {
     "V0": "raw counts",
@@ -161,7 +154,7 @@ class StationTrace(Trace):
                     f"{header['channel']}"
                 )
                 start_time = header["starttime"]
-                (response, standard, coords, format_specific) = _stats_from_inventory(
+                response, standard, coords, format_specific = _stats_from_inventory(
                     data, inventory, seed_id, start_time
                 )
                 header["response"] = response
@@ -736,7 +729,7 @@ class StationTrace(Trace):
         if "output_units" in prov_attributes.keys():
             self.stats.standard.units = prov_attributes["output_units"]
             try:
-                self.stats.standard.units_type = REVERSE_UNITS[
+                self.stats.standard.units_type = constants.REVERSE_UNITS[
                     prov_attributes["output_units"]
                 ]
             except BaseException:
@@ -1050,7 +1043,7 @@ def _stats_from_header(header, config):
         if "units_type" not in standard or standard["units_type"] == "":
             utype = get_units_type(header["channel"])
             standard["units_type"] = utype
-            standard["units"] = UNITS[utype]
+            standard["units"] = constants.UNIT_TYPES[utype]
         standard["comments"] = ""
         standard["station_name"] = ""
         standard["station_name"] = header["station"]

src/gmprocess/io/cosmos/cosmos_writer.py
@@ -11,7 +11,7 @@ from enum import Enum
 import numpy as np
 import pandas as pd
 import scipy.constants as sp
-from gmprocess.core.stationtrace import UNITS
+from gmprocess.utils.constants import UNIT_TYPES
 
 # local imports
 from gmprocess.io.asdf.stream_workspace import StreamWorkspace
@@ -253,7 +253,6 @@ class TextHeader(object):
         excelfile = pathlib.Path(datadir) / "cosmos_table4.xls"
         table4 = Table4(excelfile)
         # fill in data for text header
-        # UNITS = {"acc": "cm/s^2", "vel": "cm/s"}
         quantity = "velocity"
         if trace.stats.standard.units_type == "acc":
             quantity = "acceleration"
@@ -353,7 +352,7 @@ class TextHeader(object):
         if volume == Volume.RAW:
             self.set_header_value("raw_maximum", trace.max())
             self.set_header_value(
-                "raw_maximum_units", UNITS[trace.stats.standard.units_type]
+                "raw_maximum_units", UNIT_TYPES[trace.stats.standard.units_type]
             )
             self.set_header_value("raw_maximum_time", maxtime)
         else:
@@ -375,7 +374,7 @@ class TextHeader(object):
         self.set_header_value("processing_agency", agency)
         self.set_header_value("data_maximum", dmax)
         self.set_header_value(
-            "data_maximum_units", UNITS[trace.stats.standard.units_type]
+            "data_maximum_units", UNIT_TYPES[trace.stats.standard.units_type]
         )
         self.set_header_value("data_maximum_time", maxtime)
 
@@ -609,7 +608,7 @@ class DataBlock(object):
             quantity = "acceleration"
         npts = len(trace.data)
         itime = int(trace.stats.endtime - trace.stats.starttime)  # duration secs
-        units = UNITS[trace.stats.standard.units_type]
+        units = UNIT_TYPES[trace.stats.standard.units_type]
         ffmt = cfmt_to_ffmt(DATA_FMT, NUM_DATA_COLS)
 
         # fill in comment fields that we use for overflow information