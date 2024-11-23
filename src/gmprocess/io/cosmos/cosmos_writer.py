dd383ec75c47ecff6d48d611f70459e729b228b1
James Smith <jamesasmith@usgs.gov>
2024-11-23 12:59:08 -0700

    FIX: convertion from counts to cm/s/s in cosmos writer

--- src/gmprocess/io/cosmos/cosmos_writer.py
+++ src/gmprocess/io/cosmos/cosmos_writer.py
@@ -21,7 +21,7 @@ from gmprocess.utils.config import get_config
 from gmprocess.utils.constants import UNIT_TYPES
 from obspy.core.utcdatetime import UTCDateTime
 from obspy.geodetics.base import gps2dist_azimuth
-from gmprocess.metrics.utils import component_to_channel
+from gmprocess.metrics.utils import component_to_channel, constants
 
 COSMOS_FORMAT = 1.2
 UTC_TIME_FMT = "%m/%d/%Y, %H:%M:%S.%f"
@@ -760,6 +760,7 @@ class DataBlock(object):
                 self.trace.stats.standard.instrument_sensitivity
             )  # counts/m/s^2
             data /= instrument_sensitivity
+            data *= constants.M_TO_CM  # Convert from m to cm
             data -= data.mean()
             # split_prov = self.trace.get_parameter("signal_split")
             # split_time = split_prov["split_time"]
