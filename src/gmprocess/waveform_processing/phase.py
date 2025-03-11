8179e6c5ff02becd6824f097d9356285c0be0506
Brad Aagaard <baagaard@usgs.gov>
2025-03-11 15:50:07 -0600

    FIX: Make error message for AR pick failure more informative.

--- src/gmprocess/waveform_processing/phase.py
+++ src/gmprocess/waveform_processing/phase.py
@@ -277,7 +277,7 @@ def pick_ar(stream, config):
     # Check if we found one of each component
     # If not, use the next picker in the order of preference
     if len(st_e) != 1 or len(st_n) != 1 or len(st_z) != 1:
-        raise BaseException("Unable to perform AR picker.")
+        raise BaseException("Could not determine horizontal and vertical components in AR picker.")
 
     minloc = ar_pick(
         st_z[0].data, st_n[0].data, st_e[0].data, st_z[0].stats.sampling_rate, **params
